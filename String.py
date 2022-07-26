bd7f84cced553437aa0c4b092a2816a2253707fd
chetanya-patil <chetanya.patil@inmobi.com>
2022-07-26 17:56:13 +0530

    string in python

---
+++ String.py
@@ -0,0 +1,39 @@
+# Strings in python
+# Slicing in python 
+
+x = "Chetanya"
+print(type(x))
+
+a = '''My Name chetanya
+My Designation is..'''
+print(a)
+
+
+y = "Experience"
+print(y[0])
+
+for c in y:
+    print(c)
+
+# in keyword
+print("er" in y)
+
+if "er" in y:
+    print("er is there in Experience")
+
+
+# Concatenation in python
+firstName = "Chetanya"
+LastName = "Patil"
+print(firstName + " " +  LastName)
+
+#Slicing
+
+s = "Chetanya"
+print(s[2:5])
+print(s[-5:-2])
+
+print(s[2:])
+
+print(s[ :4])
+
