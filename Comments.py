e34efb428dc3ed5b7322a72d8a73fc2d61ac688f
chetanya-patil <chetanya.patil@inmobi.com>
2022-07-26 12:59:03 +0530

    Comments in python

---
+++ Comments.py
@@ -0,0 +1,16 @@
+from sqlite3 import ProgrammingError
+from tkinter import Variable
+
+
+# Assign 5 value to a variable
+a = 5
+b = 4 # This is called inline comments
+print(a + b)
+
+"""
+This is the sample Program
+program about comments in python
+"""
+
+# This is a simple program 
+# simple Programming
