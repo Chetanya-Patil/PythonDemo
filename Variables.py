a6e3919d751530b30ee89dc17a9999099644a9a8
chetanya-patil <chetanya.patil@inmobi.com>
2022-07-26 13:49:12 +0530

    Variables in Python

---
+++ Variables.py
@@ -0,0 +1,33 @@
+# As python is case-sensative
+from re import A, T, X
+from tkinter import Y
+
+
+age = 24
+Age = 25
+AGE = 26
+
+Name = "Chetanya"
+
+print(type(Name))
+print(type(AGE))
+
+# Type safety is not provided by python
+
+a = 4
+a = "Patil"
+
+print(type(a))
+print(a)
+
+# Assign multiple values to multiple variables
+p,q,r = "apple",'ball',"cat"
+print(p)
+print(q)
+print(r)
+	
+# One value to multiple variable
+X = Y = Z = "fruit"
+print(X)
+print(Y)
+print(Z)
